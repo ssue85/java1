81f391dc411aa74d02f91643db0b618d7b4d79ee
DJA <DJA@DJA-PC>
2017-07-23 11:22:45 +0900

    Just test Github change a file.

--- java1/src/exam05/AccessModifier.java
+++ java1/src/exam05/AccessModifier.java
@@ -13,6 +13,8 @@ public class AccessModifier extends Modifier{
 					  // 상속을 받는다 하더라도 다른 패키지에 있으면 사용할수 없음
 		e.proNum = 3; // 같은 패키지면 사용가능. 다른 패키지라 하더라도 상속을 하면 사용가능.
 					  // 즉 상속해서만 사용하는 방식이라 생각하면 될 듯
+		
+		e.pubNum = 2;
 
 	}
 }
