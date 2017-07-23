0eb4a766619cabcf093d4856e8b77b5276a58b62
DJA <DJA@DJA-PC>
2017-07-23 16:19:48 +0900

    fdsa

--- java1/src/exam05/StringExam.java
+++ java1/src/exam05/StringExam.java
@@ -4,11 +4,20 @@ public class StringExam {
 	
 	String str = "123";
 	
+	public void changeString() {
+		//str = new String("321");
+		str = "321";
+	}
+	
 	public static void main(String[] args) {
 		StringExam se = new StringExam();
 		String str2 = "123";
 		String str3 = new String("123");
 		String str4 = "123";
+		
+		se.changeString();
+		
+		System.out.println(se.str);
 		
 		System.out.println(se.str == str2);
 		System.out.println(se.str == str3);
